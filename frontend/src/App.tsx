1b52a7cc4fe0f155587e032cc35b75a757818254
Md.Fahim <mortuza.aziz.47@gmail.com>
2026-02-17 10:40:40 +0600

    Added toaster to the main app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UserContextProvider } from "./contexts/UserContext";
 import RouteComponent from "./RouteComponent";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ export default function App() {
       <UserContextProvider>
         <BrowserRouter>
           <RouteComponent />
+          <Toaster position="top-right" reverseOrder={false} />
         </BrowserRouter>
       </UserContextProvider>
     </QueryClientProvider>
