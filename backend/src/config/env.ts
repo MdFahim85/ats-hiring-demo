475c4fe9c1a04f91dcdc74923bd3e2147e1be8e8
Md.Fahim <mortuza.aziz.47@gmail.com>
2026-02-17 18:39:52 +0600

    Removed Gemini API Key and SDK. Added GROQ API key and SDK

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -11,6 +11,9 @@ export default {
   port: parseInt(process.env.PORT) || 3000,
   jwtSecret: process.env.JWT_SECRET,
   frontend_API: process.env.FRONTEND_API,
-  gemini_API: process.env.GEMINI_API_KEY,
+  groq_API: process.env.GROQ_API_KEY,
   isProduction: process.env.NODE_ENV === "production",
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleDirectURI: process.env.GOOGLE_REDIRECT_URI,
 };
